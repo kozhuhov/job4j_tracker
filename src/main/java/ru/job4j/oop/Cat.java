c1dcad6354079a93670bf4c95b5a43875fe9adab
dkozhuhov <dkozhuhov@samokat.ru>
2020-12-27 01:12:39 +0700

    1.5. Поля объекта [#434361]

--- src/main/java/ru/job4j/oop/Cat.java
+++ src/main/java/ru/job4j/oop/Cat.java
@@ -6,7 +6,7 @@ public class Cat {
     private String name;
 
     public void show() {
-        System.out.println(this.name + " like " +this.food);
+        System.out.println(this.name + " like " + this.food);
     }
 
     public void eat(String meat) {
