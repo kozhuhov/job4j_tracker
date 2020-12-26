63e0338b4ce5557e95b494f0dbf7f52c67e8a2e2
dkozhuhov <dkozhuhov@samokat.ru>
2020-12-27 01:31:44 +0700

    1.8. Состояние объекта [#434366]

---
+++ src/main/java/ru/job4j/oop/Battery.java
@@ -0,0 +1,23 @@
+package ru.job4j.oop;
+
+public class Battery {
+    private int load;
+
+    public Battery (int load) {
+        this.load = load;
+    }
+
+    public void exchange(Battery another) {
+        another.load = another.load + this.load;
+        this.load = 0;
+    }
+
+    public static void main(String[] args) {
+        Battery first = new Battery(10);
+        Battery second = new Battery(4);
+
+        first.exchange(second);
+
+    }
+
+}
