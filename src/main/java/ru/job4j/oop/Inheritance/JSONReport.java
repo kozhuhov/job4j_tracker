963839a4863bbbf0242696fe5c1f127375be6761
dkozhuhov <dkozhuhov@samokat.ru>
2020-12-28 21:11:57 +0700

    5. Аннотация @Override [#434374]

--- src/main/java/ru/job4j/oop/Inheritance/JSONReport.java
+++ src/main/java/ru/job4j/oop/Inheritance/JSONReport.java
@@ -1,7 +1,7 @@
 package ru.job4j.oop.Inheritance;
 
 public class JSONReport extends TextReport {
-
+    @Override
     public String generate(String name, String body) {
         String rsl = "{" + System.lineSeparator();
         rsl = rsl + "   \"name\": \"" + name + "\"," + System.lineSeparator();
