c5510bdbd434b2faef985a65f812fcd8b81113ff
dkozhuhov <dkozhuhov@samokat.ru>
2021-01-03 12:43:49 +0700

    9. toString [#434388]

--- src/main/java/ru/job4j/tracker/Item.java
+++ src/main/java/ru/job4j/tracker/Item.java
@@ -20,6 +20,15 @@ public class Item {
         this.name = name;
     }
 
+    @Override
+    public String toString() {
+        return "Item{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", created=" + created +
+                '}';
+    }
+
     public LocalDateTime getCreated() {
         return created;
     }
