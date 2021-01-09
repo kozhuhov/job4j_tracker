03f9b97e631d4e870396ad8d76ab1664b655b81c
dkozhuhov <dkozhuhov@samokat.ru>
2021-01-09 13:56:39 +0700

    0.4. Пользовательские исключения. [#434331]

---
+++ src/main/java/ru/job4j/ex/ElementNotFoundException.java
@@ -0,0 +1,9 @@
+package ru.job4j.ex;
+
+import java.security.spec.ECFieldF2m;
+
+public class ElementNotFoundException extends Exception {
+    public ElementNotFoundException(String message) {
+        super(message);
+    }
+}
---
+++ src/main/java/ru/job4j/ex/FindEl.java
@@ -0,0 +1,26 @@
+package ru.job4j.ex;
+
+public class FindEl {
+    public static int indexOf(String[] value, String key) throws ElementNotFoundException {
+        int rsl = -1;
+        for (int index = 0; index < value.length; index++) {
+            if (value[index] == key) {
+                rsl = index;
+            }
+        }
+        if (rsl == -1) {
+            throw new ElementNotFoundException("Element " + key + " not found");
+        }
+
+        return rsl;
+    }
+
+    public static void main(String[] args) {
+        String[] value = {"1", "2", "3"};
+        try {
+            indexOf(value, "9");
+        } catch (ElementNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+}
